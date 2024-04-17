fed7e6a3b170aa0e45938fe1cc80470792c85e76
Nahid Sultan <mac@MacBook-Pro.local>
2024-04-17 21:49:43 +0800

    added validation.

--- main.js
+++ main.js
@@ -4,12 +4,24 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
 
     const depositAmount = document.getElementById('deposit-amount');
 
+    depositInput.value = '';
+
+    if (depositValue <= 0) {
+        alert('Please enter the amount greater than 0');
+        return;
+    }else if (isNaN(depositValue)) {
+        alert('Please enter valid input');
+        return;
+    }
+
     const preDepositAmount = depositAmount.innerText;
     const newDepositAmount = parseFloat(preDepositAmount) + parseFloat(depositValue);
 
     depositAmount.innerText = newDepositAmount;
 
-    depositInput.value = '';
+    
+
+    
 
     // balance
 
@@ -30,11 +42,6 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const withdrawValue = withdrawInput.value;
 
     const withdrawAmount = document.getElementById('withdraw-amount');
-    const preWithDrawAmount = withdrawAmount.innerText;
-
-    const newWithDrawAmount = parseFloat(withdrawValue) + parseFloat(preWithDrawAmount);
-
-    withdrawAmount.innerText = newWithDrawAmount;
 
     // clear input field
 
@@ -45,8 +52,25 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const balance = document.getElementById('total-balance');
     const preBalance = balance.innerText;
 
+    // check withdraw value less than total balance and not a number
+    if (withdrawValue > preBalance) {
+        alert('Please input the value less than total balance');
+        return;
+    }else if (isNaN(withdrawValue)) {
+        alert('Please enter the valid input');
+        return;
+    }
+
+    const preWithDrawAmount = withdrawAmount.innerText;
+
+    // total withdraw amount
+    const newWithDrawAmount = parseFloat(withdrawValue) + parseFloat(preWithDrawAmount);
+    withdrawAmount.innerText = newWithDrawAmount;
+
+    
+
+    // balance
     const newBalance = parseFloat(preBalance) - parseFloat(withdrawValue);
     balance.innerText = newBalance;
-    
 
  });
